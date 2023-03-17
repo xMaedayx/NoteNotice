24fe6319e7ae4ed1199cf8bdfa470c23c7dc47b1
xMaedayx <120540407+xMaedayx@users.noreply.github.com>
2023-03-17 12:24:09 -0700

    updates

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 // Import our modular routers for /tips and /feedback
 const notesRouter = require('./notes');
-const savingRouter = require('./saving');
-const diagnosticsRouter = require('./diagnostics');
+// const savingRouter = require('./saving');
+// const diagnosticsRouter = require('./diagnostics');
 
 const app = express();
 
 app.use('/notes', notesRouter);
-app.use('/feedback', savingRouter);
-app.use('/diagnostics', diagnosticsRouter);
+// app.use('/feedback', savingRouter);
+// app.use('/diagnostics', diagnosticsRouter);
 
 module.exports = app;

12061d36ca3a6a559c72f6b9bb8173c7be60a1e3
xMaedayx <120540407+xMaedayx@users.noreply.github.com>
2023-03-17 11:15:01 -0700

    updates

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,5 @@
-import { v4 as uuidv4 } from 'uuid';
-uuidv4(); 
+// import { v4 as uuidv4 } from 'uuid';
+// uuidv4(); 
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid'); //imports the uuid package/module in Node.js...provides a way to generate universally unique identifiers (UUIDs).
 
